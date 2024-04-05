b30119e465cb23d7dbf8717a26ac08b7a472debb
yunseobking <163399942+yunseobking@users.noreply.github.com>
2024-04-05 21:14:19 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+alphabet_list="a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+file_list=$(ls files)
+
+
+for i in $alphabet_list
+    do
+        for j in $file_list
+            do
+                if [[ $j = $i* ]]; then
+                    mv files/$j $i/
+                fi
+            done
+    done
+ 
